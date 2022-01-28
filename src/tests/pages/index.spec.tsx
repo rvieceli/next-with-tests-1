37cf83dc9c32cc71e7d63515b1d8803bbfdaa582
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2022-01-28 16:42:36 +0100

    Home page tests

---
+++ src/tests/pages/index.spec.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+
+import HomePage, { getStaticProps } from 'app/pages/index';
+
+import { stripe } from 'app/services/stripe';
+import { mocked } from 'jest-mock';
+
+jest.mock('next-auth/react', () => ({
+  useSession: () => ({
+    data: null,
+    status: 'loading',
+  }),
+}));
+jest.mock('app/services/stripe');
+
+describe('Home page', () => {
+  it('renders', async () => {
+    render(<HomePage product={{ amount: 9.9, currency: 'EUR' }} />);
+
+    expect(await screen.findByText(/€9\.90/)).toBeInTheDocument();
+  });
+
+  it('loads static props', async () => {
+    const stripeRetrievePricesMocked = mocked(stripe.prices.retrieve);
+
+    stripeRetrievePricesMocked.mockResolvedValueOnce({
+      unit_amount: 990,
+      currency: 'EUR',
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            amount: 9.9,
+            currency: 'EUR',
+          },
+        },
+      })
+    );
+  });
+});
