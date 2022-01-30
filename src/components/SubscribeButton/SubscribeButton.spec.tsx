58405106372f1d4db9a22c7296fc58d2896ef297
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2022-01-30 22:02:54 +0100

    Test subscription button click event

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -1,13 +1,15 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { mocked } from 'jest-mock';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { api } from 'app/services/api';
+import { getStripeJs } from 'app/services/stripe-js';
+import { mocked, spyOn } from 'jest-mock';
 import { useSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
-
 import { SubscribeButton } from './SubscribeButton';
 
 jest.mock('next-auth/react');
-
 jest.mock('next/router');
+jest.mock('app/services/api');
+jest.mock('app/services/stripe-js');
 
 describe('SubscribeButton component', () => {
   it('should render Subscribe Now button', () => {
@@ -59,5 +61,41 @@ describe('SubscribeButton component', () => {
     fireEvent.click(button);
 
     expect(push).toHaveBeenCalledWith('/posts');
+  });
+
+  it('should call api and redirects to stripe', async () => {
+    mocked(useSession).mockReturnValueOnce({
+      data: {
+        user: { name: 'Paid User' },
+        activeSubscription: undefined,
+        expires: '',
+      },
+      status: 'authenticated',
+    });
+
+    const sessionId = 'some-session-id';
+
+    const post = spyOn(api, 'post').mockResolvedValueOnce({
+      data: {
+        sessionId,
+      },
+    });
+
+    const redirectToCheckout = jest.fn();
+
+    mocked(getStripeJs).mockReturnValueOnce({
+      redirectToCheckout,
+    } as any);
+
+    render(<SubscribeButton />);
+
+    const button = screen.getByTestId('SubscribeNow-button');
+
+    fireEvent.click(button);
+
+    await waitFor(() => expect(post).toHaveBeenCalledWith('subscriptions'));
+    await waitFor(() =>
+      expect(redirectToCheckout).toHaveBeenCalledWith({ sessionId })
+    );
   });
 });
